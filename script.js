3f0c109e3f99fa1f99cbf2d02faa567a1564a3ba
Ziirofan <ziirofan@gmail.com>
2019-02-20 22:57:18 +0100

    site web version 1

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
+/*
+ * script.js
+ *
+ */
 
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  // Check if there are any nav burgers
-  if ($navbarBurgers.length > 0) {
+/**
+*@class PhoneMenu
+*/
+class PhoneMenu {
+  constructor() {}
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
+  /**
+  * @method adaptMenu: transform menu to mobile size 
+  */
 
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
+  static adaptMenu() {
+    document.addEventListener('DOMContentLoaded', function() {
 
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+      // Get all "navbar-burger" elements
+      var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-      });
+      // Check if there are any nav burgers
+      if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function($el) {
+          $el.addEventListener('click', function() {
+
+            // Get the target from the "data-target" attribute
+            var target = $el.dataset.target;
+            var $target = document.getElementById(target);
+
+            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+            $el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
+          });
+        });
+      }
     });
   }
-});
+}
+
+PhoneMenu.adaptMenu();
